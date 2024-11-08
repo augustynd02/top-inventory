970aec250ca80622e0192f22acabf780356d09a5
yurishe0 <yurishe00@gmail.com>
2024-11-08 14:59:00 +0100

    feat: improt routers and assign them to routes

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const indexRouter = require('./routes/indexRouter');
+const productsRouter = require('./routes/productsRouter');
+const categoriesRouter = require('./routes/categoriesRouter');
+
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static('public'))
+app.use('/', indexRouter);
+app.use('/products', productsRouter);
+app.use('/categories', categoriesRouter);
 
 app.get('/', (req, res) => res.send('works'));
 
