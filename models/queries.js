dd3122280f1eac5628483394ef5da7214b33c07e
yurishe0 <yurishe00@gmail.com>
2024-11-11 20:50:02 +0100

    refactor: remove redundant try/catch statements from the queries

--- models/queries.js
+++ models/queries.js
@@ -1,49 +1,33 @@
 const pool = require('./pool');
 
 async function getAllCategories() {
-    try {
-        const q = "SELECT * FROM categories;";
-        const { rows } = await pool.query(q);
-        return rows;
-    } catch (err) {
-        next(err)
-    }
+    const q = "SELECT * FROM categories;";
+    const { rows } = await pool.query(q);
+    return rows;
 }
 
 async function getAllProducts() {
-    try {
-        const q = "SELECT * FROM products";
-        const { rows } = await pool.query(q);
-        return rows;
-    } catch (err) {
-        next(err)
-    }
+    const q = "SELECT * FROM products";
+    const { rows } = await pool.query(q);
+    return rows;
 }
 
 async function addProduct({ name, description, quantity, price, category }) {
-        const q = `
+    const q = `
         INSERT INTO products (name, description, quantity, price, category_id)
         VALUES ($1, $2, $3, $4, (SELECT id FROM categories WHERE categories.name = $5));
         `;
-        await pool.query(q, [name, description, quantity, price, category]);
+    await pool.query(q, [name, description, quantity, price, category]);
 }
 
 async function addCategory({ name, description }) {
-    try {
-        const q = "INSERT INTO categories (name, description) VALUES ($1, $2);";
-        await pool.query(q, [name, description]);
-    } catch (err) {
-        next(err);
-    }
+    const q = "INSERT INTO categories (name, description) VALUES ($1, $2);";
+    await pool.query(q, [name, description]);
 }
 
 async function deleteProduct(name) {
-    try {
-        const q = "DELETE FROM products WHERE products.name = ($1);";
-        await pool.query(q, [name]);
-    } catch (err) {
-        next(err);
-    }
+    const q = "DELETE FROM products WHERE products.name = ($1);";
+    await pool.query(q, [name]);
 }
 
 async function deleteCategory(name) {
