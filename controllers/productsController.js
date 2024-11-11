0964810dc9332e46eebda8af5e49a1acde7118b5
yurishe0 <yurishe00@gmail.com>
2024-11-11 20:17:12 +0100

    feat: asign a category name to a product based on db relation

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,6 +4,19 @@ const productsController = {
     getProducts: async (req, res) => {
         const products = await db.getAllProducts();
         const categories = await db.getAllCategories();
+        console.log(products);
+        console.log(categories);
+        products.forEach(product => {
+            const id = product.category_id;
+            let categoryName = "";
+            categories.forEach(category => {
+                if (category.id == id) {
+                    categoryName = category.name;
+                    return;
+                }
+            })
+            product["category_name"] = categoryName;
+        })
         res.render('pages/products', { products: products, categories: categories });
     },
     addProduct: async (req, res) => {
